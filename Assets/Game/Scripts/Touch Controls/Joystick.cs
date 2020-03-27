c07684ab295b72990ce826623ae0911d1982cfef
Edelweiss <barnwal.rishi@gmail.com>
2020-03-27 03:53:17 -0400

    no message

---
+++ Assets/Game/Scripts/Touch Controls/Joystick.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Joystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    public Image container;
+    public Image joystick;
+    public Vector3 inputDirection;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputDirection = Vector3.zero;
+    }
+
+    public void OnDrag(PointerEventData data)
+    {
+        Vector2 position = Vector2.zero;
+
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            container.rectTransform,
+            data.position,
+            data.pressEventCamera,
+            out position
+            );
+
+        position.x = (position.x + 150)/ container.rectTransform.sizeDelta.x;
+        position.y = (position.y + 150)/ container.rectTransform.sizeDelta.y;
+
+        float x = (container.rectTransform.pivot.x == 1.0f) ? position.x * 2 + 1 : position.x * 2 - 1;
+        float y = (container.rectTransform.pivot.y == 1.0f) ? position.y * 2 + 1 : position.y * 2 - 1;
+
+        inputDirection = new Vector3(x, y, 0);
+        inputDirection = (inputDirection.magnitude > 1) ? inputDirection.normalized : inputDirection;
+
+        joystick.rectTransform.anchoredPosition = new Vector3 (
+            inputDirection.x * (container.rectTransform.sizeDelta.x/3),
+            inputDirection.y * (container.rectTransform.sizeDelta.y/3) 
+            );
+    }
+
+    public void OnPointerDown(PointerEventData data)
+    {
+        OnDrag(data);
+    }
+
+    public void OnPointerUp(PointerEventData data)
+    {
+        inputDirection = Vector3.zero;
+        joystick.rectTransform.anchoredPosition = Vector3.zero;
+    }
+}
---
+++ Assets/Game/Scripts/Touch Controls/PlayerController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed = 5.0f;
+	public float movementThreshold = 0.5f;
+
+	public float minimumSwipeDistance = 20.0f;
+	public float swipeTimeThreshold = 0.5f;
+
+	public Image container;
+
+	public GameObject projectile;
+
+	private Rigidbody2D _rigidbody2D;
+	private Joystick joystick;
+
+	private Vector2 downPosition;
+	private float downTime;
+
+	private Vector2 upPosition;
+	private float upTime;
+
+	void Start()
+	{
+		joystick = container.GetComponent<Joystick>();
+		_rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		JoystickInput();
+		SwipeInput();
+	}
+
+	private void JoystickInput()
+	{
+		if (joystick.inputDirection.magnitude > movementThreshold)
+		{
+			_rigidbody2D.velocity = new Vector2(joystick.inputDirection.x * speed, joystick.inputDirection.y * speed);
+		}
+		else
+		{
+			_rigidbody2D.velocity = Vector2.zero;
+		}
+	}
+
+	private void SwipeInput()
+	{
+		if(Input.touchCount > 0)
+		{
+			Touch touch = Input.touches[0];
+
+			if (touch.phase == TouchPhase.Began)
+			{
+				downPosition = touch.position;
+				upPosition = touch.position;
+
+				downTime = Time.time;
+			}
+
+			if (touch.phase == TouchPhase.Ended)
+			{
+				upPosition = touch.position;
+				Vector2 swipeDirection = (upPosition - downPosition).normalized;
+				float swipeDistance = (upPosition - downPosition).magnitude;
+
+				upTime = Time.time;
+
+				float deltaTime = upTime - downTime;
+
+				if (swipeDistance > minimumSwipeDistance && deltaTime < swipeTimeThreshold)
+				{
+					GameObject fireball = Instantiate(projectile, transform.position, Quaternion.identity);
+					fireball.GetComponent<Rigidbody2D>().velocity = new Vector2(swipeDirection.x * speed, swipeDirection.y * speed);
+				}
+			}
+		}
+	}
+
+}
